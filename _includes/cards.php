a1eeea6ffc0ab389e8cf0024c4646643e6ac295f
KevinDavoust <kevin.davoust@orange.fr>
2023-03-15 13:26:42 +0100

    added fonts and modified cards.php and main.css

--- _includes/cards.php
+++ _includes/cards.php
@@ -1,15 +1,18 @@
+<link rel="stylesheet" href="../assets/css/main.css">
 <div class="ms-cards">
     <div class="ms-card1">
-        <h3 class="ms-leConcept"><span class="ms-leConceptLe">Le </span><span class="ms-leConceptConcept">Concept</span></h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+        <h3 class="ms-theConcept"><span class="ms-h2-small">Le </span><span class="ms-h2-big">Concept</span></h3>
+        <p class="ms-conceptText">Lorem ipsum dolor sit amet, consectetur adipiscing
             elit. Etiam ullamcorper magna et fringilla semper.
 
             Ut sed arcu non elit vestibulum commodo in in eros. Etiam posuere, lectus eu rhoncus efficitur, velit nisl tempus purus, in pretium neque nisi vitae tortor. Quisque ut accumsan dolor.</p>
-    <img src="../assets/Images/concept1.png" alt="chef">
     </div>
-    <div class="ms-card2">
-        <h3 class="ms-laCuisine"><span class="ms-laCuisineLa">La </span> <span class="ms-laCuisineCuisine">Cuisine</span></h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+    <img class="ms-image-card1"  src="../assets/Images/concept1.png" alt="chef">
+
+    <img class="ms-image-card2" src="../assets/Images/cuisine1.png" alt="cuisine">
+        <div class="ms-card2">
+        <h3 class="ms-theFood"><span class="ms-h2-small">La </span> <span class="ms-h2-big">Cuisine</span></h3>
+        <p class="ms-foodText">Lorem ipsum dolor sit amet, consectetur adipiscing
             elit. Etiam ullamcorper magna et fringilla semper.
 
             Ut sed arcu non elit vestibulum commodo in in eros. Etiam posuere, lectus eu rhoncus efficitur, velit nisl tempus purus, in pretium neque nisi vitae tortor. Quisque ut accumsan dolor.</p>
