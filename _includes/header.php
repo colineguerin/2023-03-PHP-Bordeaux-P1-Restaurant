91643c81c59cf0189f74bf2d7281c3f4d20d5d01
Giny23 <vduquenoy23@gmail.com>
2023-03-15 14:10:37 +0100

    1er changement

--- _includes/header.php
+++ _includes/header.php
@@ -1,1 +1,15 @@
-<?php
+<?php ?>
+<html>
+<link rel="stylesheet" href="header.css">
+    <header>
+        <nav>
+            <a class="logo"></a>
+            <ul>
+                <li>Concept</li>
+                <li>Carte</li>
+                <li>Réserver</li>
+                <li>Infos</li>
+            </ul>
+        </nav>
+    </header>
+</html>
