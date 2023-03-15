59f89c3453abde26189dcccbc8264065e62cb8e9
KevinDavoust <kevin.davoust@orange.fr>
2023-03-15 11:47:01 +0100

    added images, wireframe, started code for cards.php

--- _includes/cards.php
+++ _includes/cards.php
@@ -1,1 +1,19 @@
-<?php
+<div class="ms-cards">
+    <div class="ms-card1">
+        <h3 class="ms-leConcept"><span class="ms-leConceptLe">Le </span><span class="ms-leConceptConcept">Concept</span></h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit. Etiam ullamcorper magna et fringilla semper.
+
+            Ut sed arcu non elit vestibulum commodo in in eros. Etiam posuere, lectus eu rhoncus efficitur, velit nisl tempus purus, in pretium neque nisi vitae tortor. Quisque ut accumsan dolor.</p>
+    <img src="../assets/Images/concept1.png" alt="chef">
+    </div>
+    <div class="ms-card2">
+        <h3 class="ms-laCuisine"><span class="ms-laCuisineLa">La </span> <span class="ms-laCuisineCuisine">Cuisine</span></h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit. Etiam ullamcorper magna et fringilla semper.
+
+            Ut sed arcu non elit vestibulum commodo in in eros. Etiam posuere, lectus eu rhoncus efficitur, velit nisl tempus purus, in pretium neque nisi vitae tortor. Quisque ut accumsan dolor.</p>
+    </div>
+
+</div>
+
