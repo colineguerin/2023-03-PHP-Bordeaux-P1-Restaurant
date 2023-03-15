00a193d24d38112113b2355d8a5bb17a707ea345
Urzaag <remi.meslin.gm@gmail.com>
2023-03-15 13:24:48 +0100

    html structure ended started css

--- _includes/form.php
+++ _includes/form.php
@@ -1,12 +1,23 @@
 <?php
 
     /*
-     * php closed to start html
+     * php fermé pour commencer html
      */
 
 ?>
 
-<form>
-    <input type="text" id="ms-name" name="ms-name" placeholder="Nom ou société">
-    <input type="tel" id="ms-phone" name="ms-name" placeholder="Téléphoné">
-</form>
+<link rel="stylesheet" href="../Assets/CSS/main.css">
+<section class="ms-booking">
+    <h2><span class="ms-h2-small">Pour</span><span class="ms-h2-big"> Réserver</span></h2>
+
+    <form action="" class="ms-form-booking">
+        <input type="text" id="ms-name" name="ms-name" placeholder="Nom ou société">
+        <input type="tel" id="ms-phone" name="ms-phone" placeholder="Téléphone">
+        <input type="email" id="ms-mail" name="ms-mail" placeholder="E-mail">
+        <input type="number" id="ms-plates" name="ms-plates" placeholder="Nombre de couverts">
+        <input type="date" id="ms-date" name="ms-date" placeholder="Date">
+        <input type="time" id="ms-hour" name="ms-hour" placeholder="Heure">
+        <textarea id="ms-demands" name="ms-demands" rows="5" cols="50" placeholder="Demandes particulières"></textarea>
+        <input type="submit" value="Réserver">
+    </form>
+</section>
