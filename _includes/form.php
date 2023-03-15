6eef6e09ede375d46eb919dc31e7a197b3bf5e55
Urzaag <remi.meslin.gm@gmail.com>
2023-03-15 11:45:32 +0100

    Form creation started

--- _includes/form.php
+++ _includes/form.php
@@ -1,1 +1,12 @@
 <?php
+
+    /*
+     * php closed to start html
+     */
+
+?>
+
+<form>
+    <input type="text" id="ms-name" name="ms-name" placeholder="Nom ou société">
+    <input type="tel" id="ms-phone" name="ms-name" placeholder="Téléphoné">
+</form>
