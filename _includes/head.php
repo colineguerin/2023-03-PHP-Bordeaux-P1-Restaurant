8bc16ab763eb1004268a4d7934aae20f2ce1a6b7
colineguerin <guerin.coline@gmail.com>
2023-03-15 11:39:26 +0100

    ajout du head

--- _includes/head.php
+++ _includes/head.php
@@ -1,1 +1,13 @@
-<?php
+<?php ?>
+<<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../Assets/CSS/main.css">
+    <title>Mille Sabords</title>
+</head>
+
+</html>
