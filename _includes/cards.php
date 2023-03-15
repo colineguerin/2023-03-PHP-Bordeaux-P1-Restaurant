8c4768f9d86ccd96ec36235a427822a556f96a46
KevinDavoust <kevin.davoust@orange.fr>
2023-03-15 14:51:08 +0100

    really finished cards

--- _includes/cards.php
+++ _includes/cards.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="../assets/css/main.css">
 <div class="ms-cards">
     <div class="ms-card1">
-        <h3 class="ms-theConcept"><span class="ms-h2-small">Le </span><span class="ms-h2-big">Concept</span></h3>
+        <h3 class="ms-theConcept"><span class="ms-h2-small-card">Le </span><span class="ms-h2-big-card">Concept</span></h3>
         <p class="ms-conceptText">Lorem ipsum dolor sit amet, consectetur adipiscing
             elit. Etiam ullamcorper magna et fringilla semper.
 
@@ -11,7 +11,7 @@
 
     <img class="ms-image-card2" src="../assets/Images/cuisine1.png" alt="cuisine">
         <div class="ms-card2">
-        <h3 class="ms-theFood"><span class="ms-h2-small">La </span> <span class="ms-h2-big">Cuisine</span></h3>
+        <h3 class="ms-theFood"><span class="ms-h2-small-card">La </span> <span class="ms-h2-big-card">Cuisine</span></h3>
         <p class="ms-foodText">Lorem ipsum dolor sit amet, consectetur adipiscing
             elit. Etiam ullamcorper magna et fringilla semper.
 
