1ada36fbfa6a24021ee5eecb736fe2f02adb10f4
KevinDavoust <kevin.davoust@orange.fr>
2023-03-15 15:02:39 +0100

    linked all stylesheets to index.php

--- _includes/head.php
+++ _includes/head.php
@@ -1,5 +1,5 @@
 <?php ?>
-<<!doctype html>
+<!doctype html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
@@ -7,6 +7,16 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../Assets/CSS/main.css">
+    <link rel="stylesheet" href="../Assets/CSS/Modules/variables.css">
+    <link rel="stylesheet" href="../Assets/CSS/Modules/header.css">
+    <link rel="stylesheet" href="../Assets/CSS/Modules/cards.css">
+    <link rel="stylesheet" href="../Assets/CSS/Modules/carrousel.css">
+    <link rel="stylesheet" href="../Assets/CSS/Modules/menu.css">
+    <link rel="stylesheet" href="../Assets/CSS/Modules/form.css">
+    <link rel="stylesheet" href="../Assets/CSS/Modules/button.css">
+    <link rel="stylesheet" href="../Assets/CSS/Modules/contact.css">
+    <link rel="stylesheet" href="../Assets/CSS/Modules/footer.css">
+
     <title>Mille Sabords</title>
 </head>
 
--- index.php
+++ index.php
@@ -4,22 +4,22 @@ include "_includes/head.php"
 ?> <h1>Test</h1>
 <?php
 //fichier php de la page accueil avec barre de navigation et photo
-include "_includes/header.php"
+include "_includes/header.php";
 
 //fichier php des cards avec (philosophie + photo) et (cuisine + photo)
-include "_includes/cards.php"
+include "_includes/cards.php";
 
 //fichier php du carrousel des photos de cuisine
-include "_includes/carrousel.php"
+include "_includes/carrousel.php";
 
 //fichier php du carrousel du menu
-include "_includes/menu.php"
+include "_includes/menu.php";
 
 //fichier php du formulaire de contact
-include "_includes/form.php"
+include "_includes/form.php";
 
 //fichier php de la partie contact avec map + téléphone + réseaux
-include "_includes/contact.php"
+include "_includes/contact.php";
 
 //fichier php du footer avec liens obligatoires
 include "_includes/footer.php"
