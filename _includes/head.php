9ab696a837f6e448654b2d974109a89d5b8ad6c8
KevinDavoust <kevin.davoust@orange.fr>
2023-03-16 09:03:14 +0100

    oui oui

--- _includes/head.php
+++ _includes/head.php
@@ -7,16 +7,6 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../Assets/CSS/main.css">
-    <link rel="stylesheet" href="../Assets/CSS/Modules/variables.css">
-    <link rel="stylesheet" href="../Assets/CSS/Modules/header.css">
-    <link rel="stylesheet" href="../Assets/CSS/Modules/cards.css">
-    <link rel="stylesheet" href="../Assets/CSS/Modules/carrousel.css">
-    <link rel="stylesheet" href="../Assets/CSS/Modules/menu.css">
-    <link rel="stylesheet" href="../Assets/CSS/Modules/form.css">
-    <link rel="stylesheet" href="../Assets/CSS/Modules/button.css">
-    <link rel="stylesheet" href="../Assets/CSS/Modules/contact.css">
-    <link rel="stylesheet" href="../Assets/CSS/Modules/footer.css">
-
     <title>Mille Sabords</title>
 </head>
 
