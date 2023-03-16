28d1ac581cca735bb50879b06e34eed99fa7e6cc
Urzaag <remi.meslin.gm@gmail.com>
2023-03-16 10:43:10 +0100

    Form requirements added

--- _includes/form.php
+++ _includes/form.php
@@ -14,12 +14,12 @@
     <h2><span class="ms-h2-small">Pour</span><span class="ms-h2-big"> Réserver</span></h2>
 
     <form action="" class="ms-form-booking">
-        <input type="text" class="ms-name" name="ms-name" placeholder="Nom ou société">
-        <input type="tel" class="ms-phone" name="ms-phone" placeholder="Téléphone">
+        <input type="text" class="ms-name" name="ms-name" placeholder="Nom ou société *" required>
+        <input type="tel" class="ms-phone" name="ms-phone" placeholder="Téléphone *" required>
         <input type="email" class="ms-mail" name="ms-mail" placeholder="E-mail">
-        <input type="number" class="ms-plates" name="ms-plates" placeholder="Nombre de couverts">
-        <input type="date" class="ms-date" name="ms-date" value="<?= date("Y-m-d");?>" min="<?= date("Y-m-d");?>">
-        <input type="time" class="ms-hour" name="ms-hour" value="<?php date_default_timezone_set('Europe/Paris'); echo date("H:i");?>" list="hoursList">
+        <input type="number" class="ms-plates" name="ms-plates" placeholder="Nombre de couverts *" required>
+        <input type="date" class="ms-date" name="ms-date" value="<?= date("Y-m-d");?>" min="<?= date("Y-m-d");?>" required>
+        <input type="time" class="ms-hour" name="ms-hour" value="<?php date_default_timezone_set('Europe/Paris'); echo date("H:i");?>" list="hoursList" required>
 
         <!-- Datalist to enable only certain hours to book a table -->
         <datalist id="hoursList">
