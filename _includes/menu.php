5a35cfcf18a7070b1616dc3a9f445f51f6d50e8a
colineguerin <guerin.coline@gmail.com>
2023-03-16 16:15:19 +0100

    menu fiinall version desktop

--- _includes/menu.php
+++ _includes/menu.php
@@ -2,19 +2,21 @@
 
 <section class="ms-menu">
 
-    <h2 class="ms-title-menu-section"><span class="ms-h2-small">La </span><span class="ms-h2-big"> Carte</span></h2>
-
+    <h2 class="ms-menu-section-title"><span class="ms-h2-small">La</span><span class="ms-h2-big"> Carte</span></h2>
     <div class="ms-menu-wrapper">
-        <button class="ms-menu-previous"></button>
-        <nav class="ms-menu-nav">
-            <ul class="ms-menu-list">
-                <li class="ms-menu-items" onclick="openStarters()" id="startersItem">Entrées</li>
-                <li class="ms-menu-items" onclick="openMains()" id="mainsItem">Plats</li>
-                <li class="ms-menu-items" onclick="openDesserts()" id="dessertsItem">Desserts</li>
-            </ul>
-        </nav>
-        <div class="ms-menu-content">
-            <div class="ms-menu-type" id="startersContent">
+        <div class="ms-previous-button-wrapper">
+            <button id="previousButton" onclick="openPrevious()"></button>
+        </div>
+        <div class="menu-container">
+            <nav class="ms-menu-nav">
+                <ul class="ms-menu-list">
+                    <li class="ms-menu-items" onclick="openStarters()" id="startersItem">Entrées</li>
+                    <li class="ms-menu-items" onclick="openMains()" id="mainsItem">Plats</li>
+                    <li class="ms-menu-items" onclick="openDesserts()" id="dessertsItem">Desserts</li>
+                </ul>
+            </nav>
+            <div class="ms-menu-content">
+                <div class="ms-menu-type" id="startersContent">
                 <p class="ms-dish-label">Mousse de truite gravlax, betterave fumée et pickles de graines de moutarde</p>
                 <p class="ms-dish-price">11 €</p>
                 <p class="ms-dish-transition">____</p>
@@ -24,10 +26,10 @@
                 <p class="ms-dish-label">Assiette de coquillages et son assortiment de sauces maison</p>
                 <p class="ms-dish-price">14 €</p>
                 <p class="ms-dish-transition">____</p>
-                <p class="ms-dish-label">Mousse de truite gravlax, betterave fumée et pickles de graines de moutarde</p>
-                <p class="ms-dish-price">11 €</p>
+                <p class="ms-dish-label">Aumonière au saumon et poireau, toasts croustillants au chèvre frais</p>
+                <p class="ms-dish-price">8 €</p>
             </div>
-            <div class="ms-menu-type" id="mainsContent">
+                <div class="ms-menu-type" id="mainsContent">
                 <p class="ms-dish-label">Filet de brochet sauce yuzu, confit de tomates à la provençale, pickles
                     d'oignons grillés</p>
                 <p class="ms-dish-price">21 €</p>
@@ -40,11 +42,10 @@
                     pousses d'épinards</p>
                 <p class="ms-dish-price">23 €</p>
                 <p class="ms-dish-transition">____</p>
-                <p class="ms-dish-label">Truite farcie en robe de courgette, beurre blanc, mousseline de panais et
-                    pousses d'épinards</p>
+                <p class="ms-dish-label">Ceviche de bar de ligne sablé au parmesan et poêlée de légumes printaniers à l'huile d'olive</p>
                 <p class="ms-dish-price">23 €</p>
             </div>
-            <div class="ms-menu-type" id="dessertsContent">
+                <div class="ms-menu-type" id="dessertsContent">
                 <p class="ms-dish-label">Sélection de fromages du chef</p>
                 <p class="ms-dish-price">10 €</p>
                 <p class="ms-dish-transition">____</p>
@@ -52,16 +53,19 @@
                 <p class="ms-dish-price">9 €</p>
                 <p class="ms-dish-transition">____</p>
                 <p class="ms-dish-label">Sablé breton, crémeux gingembre et éclats de meringue aromatisée au citron vert</p>
-                <p class="ms-dish-price">9 €</p>
+                <p class="ms-dish-price">8 €</p>
                 <p class="ms-dish-transition">____</p>
-                <p class="ms-dish-label">Sablé breton, crémeux gingembre et éclats de meringue aromatisée au citron vert</p>
-                <p class="ms-dish-price">9 €</p>
+                <p class="ms-dish-label">Sphère aux trois chocolats, espuma de crème anglaise et amandes concassées</p>
+                <p class="ms-dish-price">10 €</p>
+            </div>
             </div>
         </div>
+        <div class="ms-next-button-wrapper">
+            <button id="nextButton" onclick="openNext()"></button>
+        </div>
     </div>
-    <button class="ms-menu-next"></button>
     <div class="ms-menu-below">
-        <p>noeud</p>
+        <img src="../Assets/Images/noeud.png" class="ms-knot-transition" alt="image d'un noeud">
     </div>
 </section>
 
@@ -87,6 +91,7 @@
     }
     */
 
+    // Defining all my variables
     let i = 1;
     let startersItem = document.querySelector("#startersItem");
     let mainsItem = document.querySelector("#mainsItem");
@@ -95,44 +100,87 @@
     let mainsContent = document.querySelector("#mainsContent");
     let dessertsContent = document.querySelector("#dessertsContent");
 
+    // Defining style when user clicks on "Entrées"
     function openStarters() {
         startersItem.style.backgroundColor = "#E1DBCA";
         startersItem.style.color = "#102839";
+        startersItem.style.height = "72px";
+        startersItem.style.paddingTop = "16px";
         mainsItem.style.backgroundColor = "#102839";
         mainsItem.style.color = "#E1DBCA";
+        mainsItem.style.height = "56px";
+        mainsItem.style.paddingTop = "8px";
         dessertsItem.style.backgroundColor = "#102839";
         dessertsItem.style.color = "#E1DBCA";
+        dessertsItem.style.height = "56px";
+        dessertsItem.style.paddingTop = "8px";
         startersContent.style.display = "block";
         mainsContent.style.display = "none";
         dessertsContent.style.display = "none";
+        i = 1;
     }
 
+    // Defining style when user clicks on "Plats"
     function openMains() {
         mainsItem.style.backgroundColor = "#E1DBCA";
         mainsItem.style.color = "#102839";
+        mainsItem.style.height = "72px";
+        mainsItem.style.paddingTop = "16px";
         startersItem.style.backgroundColor = "#102839";
         startersItem.style.color = "#E1DBCA";
+        startersItem.style.height = "56px";
+        startersItem.style.paddingTop = "8px";
         dessertsItem.style.backgroundColor = "#102839";
         dessertsItem.style.color = "#E1DBCA";
+        dessertsItem.style.height = "56px";
+        dessertsItem.style.paddingTop = "8px";
         mainsContent.style.display = "block";
         startersContent.style.display = "none";
         dessertsContent.style.display = "none";
+        i = 2;
     }
 
+    // Defining style when user clicks on "Desserts"
     function openDesserts() {
         dessertsItem.style.backgroundColor = "#E1DBCA";
         dessertsItem.style.color = "#102839";
+        dessertsItem.style.height = "72px";
+        dessertsItem.style.paddingTop = "16px";
         startersItem.style.backgroundColor = "#102839";
         startersItem.style.color = "#E1DBCA";
+        startersItem.style.height = "56px";
+        startersItem.style.paddingTop = "8px";
         mainsItem.style.backgroundColor = "#102839";
         mainsItem.style.color = "#E1DBCA";
+        mainsItem.style.height = "56px";
+        mainsItem.style.paddingTop = "8px";
         dessertsContent.style.display = "block";
         startersContent.style.display = "none";
         mainsContent.style.display = "none";
+        i = 3;
     }
 
-
-
-
+    function openPrevious() {
+        if(i === 1) {
+            openDesserts();
+        }
+        else if(i === 2) {
+            openStarters();
+        }
+        else {
+            openMains();
+        }
+    }
+    function openNext() {
+        if(i === 1) {
+            openMains();
+        }
+        else if(i === 2) {
+            openDesserts();
+        }
+        else {
+            openStarters();
+        }
+    }
 
 </script>
