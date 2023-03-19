45adb02db31fe86d6c7057a7661ba2559ba56cb2
KevinDavoust <kevin.davoust@orange.fr>
2023-03-19 19:42:47 +0100

    adjusted contact for responsive, added padding to H1

--- _includes/cards.php
+++ _includes/cards.php
@@ -4,14 +4,14 @@
         <h3 class="ms-theConcept"><span class="ms-h2-small-card">Le </span><span class="ms-h2-big-card">Concept</span></h3>
         <p class="ms-conceptText">Depuis plus de vingt ans, le Mille Sabords sillone les eaux tumultueuses de la Garonne à la recherche des meilleurs produits pour régaler vos papilles. Aujourd'hui, nous avons jeté l'ancre au port de Bordeaux, mais qui sait ? Demain, nous serons peut-être à Agen. Venez vite nous rendre visite avant que le Mille Sabords ne largue les amarres !</p>
     </div>
-    <img class="ms-image-card1"  src="../assets/Images/concept1.png" alt="chef">
+    <img class="ms-image-card1"  src="../Assets/Images/concept1.png" alt="chef">
 
         <div class="ms-card2">
         <h3 class="ms-theFood"><span class="ms-h2-small-card">La </span> <span class="ms-h2-big-card">Cuisine</span></h3>
         <p class="ms-foodText">Notre chef-capitaine Jean Bokuistot sélectionne pour vous les meilleurs produits, selon des variables de saison et d'origine définies avec soin. Sa cuisine, essentiellement orientée objet, ainsi que la structure remarquable de ses plats en font de véritables tableaux culinaires. Accompagné de son crew d'excellence, Jean Bokuistot ne recule jamais devant un challenge. Comme il aime le répéter en boucle : "le principal, c'est que ça marche !"
         </p>
     </div>
-    <img class="ms-image-card2" src="../assets/Images/cuisine1.png" alt="cuisine">
+    <img class="ms-image-card2" src="../Assets/Images/cuisine1.png" alt="cuisine">
 
 </section>
 
