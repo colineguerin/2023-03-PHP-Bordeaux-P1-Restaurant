5a3b0f22435c0544d7a0a1f34d5fbb07742832cc
Urzaag <remi.meslin.gm@gmail.com>
2023-03-22 13:43:21 +0100

    commented front-end form errors

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,3 +1,7 @@
+/*
+Transition function to trigger animations on carousel, menu, form and contact sections
+ */
+
 function transition() {
     let reveals = document.querySelectorAll(".transitioned");
     for (let i = 0; i < reveals.length; i++)
@@ -8,11 +12,61 @@ function transition() {
 
         if (elementTop < windowHeight - elementVisible){
             reveals[i].classList.add("active");
-            console.log
-            //".ms-card2-trans2".classList.add("active");
         } else {
             reveals[i].classList.remove("active");
         }
     }
 }
 window.addEventListener("scroll", transition);
+
+
+/*
+Function to trigger errors in form
+ */
+/*
+const form = document.querySelector('form');
+const email = document.querySelector('.ms-mail');
+
+email.addEventListener("input",  (event) => {
+    if (email.validity.valid) {
+        emailError.textContent = "";
+        emailError.className = "error";
+    } else {
+        showError();
+    }
+});
+*/
+//TODO : add toasts if email is not valid on form submission
+/*
+email.addEventListener( "invalid",
+    function( event ) {
+        event.preventDefault();
+    }, true);
+
+form.addEventListener("submit", function (event) {
+    if (!email.validity.valid) {
+        showError();
+        event.preventDefault();
+    }
+});
+
+function showError() {
+    if (email.validity.valueMissing) {
+        emailError.textContent = "Veuillez entrer une adresse email.";
+    } else if (email.validity.typeMismatch){
+        Toastify({
+        text: "This is a toast",
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "left", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        }
+    }).showToast();
+    }
+    //emailError.className = "error active";
+}
+*/
