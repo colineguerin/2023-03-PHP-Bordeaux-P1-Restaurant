cf72a7dc34052207e5791260290b99f111567b87
KevinDavoust <kevin.davoust@orange.fr>
2023-03-22 14:55:15 +0100

    modif footer et autres

--- _includes/footer.php
+++ _includes/footer.php
@@ -3,6 +3,5 @@
             <p>© Mille Sabords 2023</p>
             <a href="../Ressources/politiqueConfidentitalite.pdf">Politique de confidentialité</a>
         </div>
-        </div>
 
 </footer>
